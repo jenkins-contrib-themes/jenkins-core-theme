ecca281aaab4edd55987b6becbb26b16e276206e
Rodrigo Feijolo <r.feijolo@gmail.com>
2016-02-17 20:52:53 -0200

    Adicionando projeto Web.API

---
+++ src/GetCompliance.Services.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace GetCompliance.Services.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/GetCompliance.Services.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetCompliance.Services.API.WebApiApplication" Language="C#" %>
