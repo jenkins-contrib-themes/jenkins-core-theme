1cefc94b9cf41454fba2e04bed04a397bd70782c
Thiago Sciotta <thiagog3@gmail.com>
2016-02-21 11:53:11 -0300

    Incluindo task browser-sync e opção de sourcemap para o sass

--- src/GetCompliance.Presentation.UI/Gruntfile.js
+++ src/GetCompliance.Presentation.UI/Gruntfile.js
@@ -4,17 +4,42 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          style: 'expanded'
+          style: 'expanded',
+          sourcemap: 'file'
         },
         files: {
           'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: "scss/*.scss",
+        tasks: ['sass']
+      }
+    },
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "css/*.css",
+            "*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
 
 };
